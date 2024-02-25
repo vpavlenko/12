c444ef746da6d0526e799bc220ac7635bf5bda9c
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-02-25 11:04:55 +0400

    Add wjd maj blues extract

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import data from "../data/wjd_maj_blues.json";
+
+type JazzSolo = {
+  melid: number;
+  trackid: number;
+  compid: number;
+  recordid: number;
+  performer: string;
+  title: string;
+  titleaddon: string;
+  solopart: number;
+  instrument: string;
+  style: string;
+  avgtempo: number;
+  tempoclass: string;
+  rhythmfeel: string;
+  key: string;
+  signature: string;
+  chord_changes: string;
+  chorus_count: number;
+  composer: string;
+  form: string;
+  template: string;
+  tonalitytype: string;
+  genre: string;
+};
+
+const solos: JazzSolo[] = data;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {solos.map(({ title, performer, key, chord_changes, style }) => (
+        <div>
+          {title} {performer} {key} {chord_changes} {style}
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
