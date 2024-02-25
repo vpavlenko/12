baf87e404fabaf14c73e034f5c5a8033a5a39310
Vitaly Pavlenko <cxielamiko@gmail.com>
2024-02-25 13:26:36 +0400

    Show CSV content for a selected solo

--- README.md
+++ README.md
@@ -1,30 +1,12 @@
-# React + TypeScript + Vite
+# 12
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
+```sql
+SELECT *
+FROM solo_info
+JOIN composition_info ON solo_info.compid = composition_info.compid
+WHERE composition_info.tonalitytype = 'BLUES'
+AND composition_info.form = 'A12' AND solo_info.signature = '4/4'
+AND solo_info.key LIKE '%maj';
 ```
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+on [wjazzd.db](https://jazzomat.hfm-weimar.de/dbformat/dboverview.html)
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import data from "../data/wjd_maj_blues.json";
+import Papa from "papaparse";
 
 type JazzSolo = {
   melid: number;
@@ -27,16 +28,48 @@ type JazzSolo = {
   genre: string;
 };
 
+const makeCsvMelodyFileName = ({ title, performer }: JazzSolo) =>
+  performer.replace(/ /g, "") + "_" + title.replace(/ /g, "") + "_Solo.csv";
+
+const CsvMelodyLoader: React.FC<{ fileName: string }> = ({ fileName }) => {
+  const [data, setData] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    const filePath = `csv_melody/${fileName}`;
+
+    fetch(filePath)
+      .then((response) => response.text())
+      .then((csvText) => {
+        Papa.parse(csvText, {
+          complete: (result: any) => {
+            setData(result.data);
+          },
+          header: true,
+        });
+      })
+      .catch((error) => console.error("Error loading the CSV file:", error));
+  }, [fileName]);
+
+  return <div>{JSON.stringify(data)}</div>;
+};
+
 const solos: JazzSolo[] = data;
 
 function App() {
+  const [selectedSolo, setSelectedSolo] = useState(0);
   return (
     <>
-      {solos.map(({ title, performer, key, chord_changes, style }) => (
-        <div>
-          {title} {performer} {key} {chord_changes} {style}
-        </div>
+      {solos.map(({ title }, index) => (
+        <>
+          <span onClick={() => setSelectedSolo(index)}>{title}</span>{" "}
+        </>
       ))}
+      <div>
+        Selected: {solos[selectedSolo].title} {solos[selectedSolo].performer}{" "}
+        {solos[selectedSolo].key} {solos[selectedSolo].chord_changes}{" "}
+        {solos[selectedSolo].style}
+      </div>
+      <CsvMelodyLoader fileName={makeCsvMelodyFileName(solos[selectedSolo])} />
     </>
   );
 }
